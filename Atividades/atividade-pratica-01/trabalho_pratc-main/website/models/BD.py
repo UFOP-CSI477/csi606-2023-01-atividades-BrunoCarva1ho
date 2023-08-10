a6efe06589ad95dd2fb29bddaa1d55b78ababb8a
Bruno Carvalho <86502087+BrunoCarva1ho@users.noreply.github.com>
2023-08-10 15:53:52 -0300

    Add files via upload
    Não tinha conseguido upar os arquivos completos, porém foi tudo feito e entregue no dia combinado, tanto que upei esses mesmo arquivos nos repositórios do meu perfil na data especificada.
    Estou tentando agora com um computador da ufop.

---
+++ Atividades/atividade-pratica-01/trabalho_pratc-main/website/models/BD.py
@@ -0,0 +1,7 @@
+import pymysql
+
+mysql = pymysql.connect(host='localhost',
+                        port=3306,
+                        user='Bruno',
+                        passwd='Passeivoado1',
+                        database='sistemasWeb1')
